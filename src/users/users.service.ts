b55bfd9138c9a67a74943969369bb2d3e484ca6f
alexcode117 <alexcode7vzla@gmail.com>
2025-05-25 19:57:14 -0400

    Adding services, controller and module users

---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: { email: string; password: string; name: string }) {
+    return this.prisma.user.create({ data });
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findById(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async updateRefreshToken(userId: number, refreshToken: string | null) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { refreshToken },
+    });
+  }
+}
